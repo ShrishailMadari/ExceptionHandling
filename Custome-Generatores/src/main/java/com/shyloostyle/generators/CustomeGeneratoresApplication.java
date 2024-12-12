b59dc2120983b69fabde13c63878579777c186da
shrishail madari <shrishailmadari@gmail.com>
2024-12-12 19:01:45 +0530

    add these files

---
+++ Custome-Generatores/src/main/java/com/shyloostyle/generators/CustomeGeneratoresApplication.java
@@ -0,0 +1,13 @@
+package com.shyloostyle.generators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomeGeneratoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomeGeneratoresApplication.class, args);
+	}
+
+}
